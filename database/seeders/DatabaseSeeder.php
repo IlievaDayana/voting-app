9e192336a02d6855750dfde60b0627495dba8ced
IlievaDayana <73341895+IlievaDayana@users.noreply.github.com>
2021-10-02 20:59:41 +0300

    category table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Idea;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::factory()->create(['name'=> 'Category 1']);
+        Category::factory()->create(['name'=> 'Category 2']);
+        Category::factory()->create(['name'=> 'Category 3']);
+        Category::factory()->create(['name'=> 'Category 4']);
         \App\Models\User::factory(10)->create();
         Idea::factory(30)->create();
+        
+
     }
 }
